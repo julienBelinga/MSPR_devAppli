6a31142c19c217d4c56b55998fbd0cec90036cb7
tdassise <77011334+tdassise@users.noreply.github.com>
2022-02-23 21:07:23 +0100

    Update Gestionhtml.java

--- src/main/java/Gestionhtml.java
+++ src/main/java/Gestionhtml.java
@@ -1,14 +1,10 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Gestionhtml {
-
-
-
-
-
-
 
     public String GenererLiensAccueil() throws IOException {
         BufferedReader in = new BufferedReader(new FileReader("src/main/Txt/staff.txt"));
@@ -20,47 +16,47 @@ public class Gestionhtml {
         }
 
         java.util.Collections.sort(list);
-        String AllHref = "";
+        StringBuilder AllHref = new StringBuilder();
 
         for (int i = 0; i < list.size(); i++) {
             list.set(i, "<a href=\"Agents/" + list.get(i) + ".html\">" + list.get(i) + "</a></br>");
-            AllHref = AllHref + list.get(i) + "\n";
+            AllHref.append(list.get(i)).append("\n");
         }
 
-        return AllHref;
+        return AllHref.toString();
+    }
+
+    public void InsertionHTML(String AncienneVar, String HTMLInsert) throws IOException {
+
+        File HTMLSqueletteAccueil = new File("src/main/Html/Squelette/Accueil.html");
+        File HTMLGenerated = new File("src/main/Html/Generation/Accueil.html");
+        StringBuilder AncienHTML = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(HTMLSqueletteAccueil));
+
+        String line = reader.readLine();
+        while (line != null) {
+            AncienHTML.append(line).append(System.lineSeparator());
+            line = reader.readLine();
         }
 
-        public void InsertionHTML(String AncienneVar, String HTMLInsert) throws IOException {
+        String NouveauHTML = AncienHTML.toString().replaceAll(AncienneVar, HTMLInsert);
 
-            File HTMLSqueletteAccueil = new File("src/main/Html/Squelette/Accueil.html");
-            File HTMLGenerated = new File("src/main/Html/Generation/Accueil.html");
-            String AncienHTML = "";
-            BufferedReader reader = new BufferedReader(new FileReader(HTMLSqueletteAccueil));
+        FileWriter writer = new FileWriter(HTMLGenerated);
+        writer.write("");
+        writer.write(NouveauHTML);
+        reader.close();
+        writer.close();
 
-            String line = reader.readLine();
-            while (line != null)
-            {
-                AncienHTML = AncienHTML + line + System.lineSeparator();
-                line = reader.readLine();
-            }
-
-            String NouveauHTML = AncienHTML.replaceAll(AncienneVar, HTMLInsert);
-
-            FileWriter writer = new FileWriter(HTMLGenerated);
-            writer.write("");
-            writer.write(NouveauHTML);
-            reader.close();
-            writer.close();
-
-        }
+    }
 
 
-    public void SupprimmerAgentsAvantMAJ(){
+    public void SupprimmerAgentsAvantMAJ() {
         File DossierAgents = new File("src/main/Html/Generation/Agents");
 
         File[] ListeFichiersAgents = DossierAgents.listFiles();
-        for(File file : ListeFichiersAgents){
-            System.out.println("Suppresion de  "+ file.getName());
+        assert ListeFichiersAgents != null;
+        for (File file : ListeFichiersAgents) {
+            System.out.println("Fichier \"" + file.getName() + "\" supprimé.");
             file.delete();
         }
 
@@ -70,19 +66,18 @@ public class Gestionhtml {
         BufferedReader in = new BufferedReader(new FileReader("src/main/Txt/staff.txt"));
         String str;
 
+        // suppression de chaque agent en parcourant staff.txt
         List<String> list = new ArrayList<String>();
         SupprimmerAgentsAvantMAJ();
-        while ((str = in.readLine()) != null)
-        {
+        while ((str = in.readLine()) != null) {
             list.add(str);
         }
 
         java.util.Collections.sort(list);
         String AllHref = "";
 
-        for (int i = 0; i < list.size(); i++)
-        {
-            File HTMLGenerated = new File("src/main/Html/Generation/Agents/" + list.get(i) + ".html");
+        for (String s : list) {
+            File HTMLGenerated = new File("src/main/Html/Generation/Agents/" + s + ".html");
             FileWriter writer = new FileWriter(HTMLGenerated);
             writer.write("");
             writer.close();
@@ -90,4 +85,64 @@ public class Gestionhtml {
 
     }
 
+    public void ajouterInfosAgent() throws IOException {
+        BufferedReader staffTxt = new BufferedReader(new FileReader("src/main/Txt/staff.txt"));
+
+        // pour chaque ligne de staff.txt
+        String ligne;
+        while ((ligne = staffTxt.readLine()) != null) {
+            // on récupère le squelette HTML
+            BufferedReader squeletteHtml = new BufferedReader(new FileReader("item-agent.html"));
+            // substring sur la fin de la ligne de staff.txt pour retirer l'extension
+            ligne.substring(ligne.length() - 5);
+            // Création du fichier HTML
+            BufferedReader fichierTxt = new BufferedReader(new FileReader("src/main/Txt/" + ligne + ".txt"));
+            File fichierHtml = new File("src/main/Html/Generation/Agents/" + ligne + ".html");
+            // Création d'un writer pour remplir le fichier
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fichierHtml));
+            // La première ligne correspond au nom de l'agent
+            String nomAgent = fichierTxt.readLine();
+            // La deuxième est son prénom
+            String prenomAgent = fichierTxt.readLine();
+            // La troisième est sa mission
+            String missionAgent = fichierTxt.readLine();
+
+            // On skip le mot de passe et la ligne blanche
+            int i = 0;
+            while (i < 2) {
+                fichierTxt.readLine();
+                i++;
+            }
+
+            // On récupère l'équipement de l'agent
+            StringBuilder equipementAgent = new StringBuilder();
+            String equipement;
+            while ((equipement = fichierTxt.readLine()) != null) {
+                equipementAgent.append("<br><label>").append(equipement).append("<input type=\"checkbox\" name=\"\" checked>" + "</label>");
+            }
+
+            String ligneSquelette;
+            StringBuilder fichierAgent = new StringBuilder();
+            String strRecherche = "id=\"main-container\">";
+            // Pour chaque ligne du squelette HTML
+            while ((ligneSquelette = squeletteHtml.readLine()) != null) {
+                fichierAgent.append(ligneSquelette);
+                // On découpe la ligne en mots
+                String[] mots = ligneSquelette.split(" ");
+                // Pour chaque mot
+                for (String mot : mots) {
+                    // Si le mot actuel correspond à la string recherchée
+                    if (mot.equals(strRecherche)) {
+                        // On ajoute le nom, prénom, mission et l'équipement de l'agent dans le fichier HTML créé
+                        fichierAgent.append(nomAgent).append(" ").append(prenomAgent).append(" - ").append(missionAgent).append(" <br>");
+                        fichierAgent.append(equipementAgent);
+                    }
+                }
+            }
+            writer.write(String.valueOf(Jsoup.parse(String.valueOf(fichierAgent))));
+            writer.flush();
+            writer.close();
+        }
+        System.out.println("\nInformations des agents ajoutés");
+    }
 }
